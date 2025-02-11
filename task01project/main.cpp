6761acc97ae6fc0a64f9188be2796f113ca034ee
Sofia Woronowicz <emiliavoronovic@gmail.com>
2025-02-11 17:37:44 +0300

    Add init and convert functions

---
+++ task01project/main.cpp
@@ -0,0 +1,14 @@
+#include "util.h"
+#include <ctime>
+#define SIZE 20
+
+int main() {
+	srand(time(NULL));
+	int array[SIZE];
+
+	init(array, SIZE, -20, 20);
+
+	cout << "Array: " << convert(array, SIZE) << endl;
+
+	return 0;
+}
--- task01project/test01.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-	cout << "testing..." << endl;
-	return 0;
-
-}
