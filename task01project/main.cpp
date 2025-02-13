dd052f3481f9845ce803af65b6d754eda7d6fc80
Sofia Woronowicz <emiliavoronovic@gmail.com>
2025-02-13 18:56:31 +0300

    Add select function

--- task01project/main.cpp
+++ task01project/main.cpp
@@ -1,17 +1,19 @@
 ﻿#include "util.h"
 #include "sort.h"
 #include <ctime>
-#define SIZE 20
+#define SIZE 10
 
 //это мощный комментарий
 int main() {
 	srand(time(NULL));
-	int array[SIZE];
+	//int array[]{ 1,2,3,4,5,6,7,8,9,10 };
+	//int array[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	init(array, SIZE, -20, 20);
+	int array[SIZE]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	//init(array, SIZE, -20, 20);
 
 	cout << "Before array: " << convert(array, SIZE) << endl;
 	bubble_sort(array, SIZE);
 	cout << "After array: " << convert(array, SIZE) << endl;
-	return 0;
 }
--- task01project/sort.cpp
+++ task01project/sort.cpp
@@ -1,14 +1,41 @@
 #include "sort.h"
+#include "util.h"
+using namespace std;
 
+// O(size * size ), O(N^2)
+// best - O(N)
 void bubble_sort(int* array, int size) {
-	for (int g = 0;  g < size - 1; g++)
+	for (int j = 0; j < size - 1 - j; j++)
 	{
-		for (int i = 0; i < size; i++) {
-		if (array[i] < array[i + 1])
+		bool flag = true;
+		for (int i = 0; i < size - 1; i++)
 		{
-			int t = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = t;
+			if (array[i] > array[i + 1]) {
+				int t = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = t;
+				flag = false;
+			}
 		}
+		if (flag) {
+			break;
+		}
+	}
+}
+void selected_sort(int* array, int size) {
+
+	for (int j = 0; j < size - 1; j++)
+	{
+
+		int index = j;
+		for (int i = j + 1; i < size; i++)
+		{
+			if (array[i] > array[index]) {
+				index = i;
+			}
+		}
+		int t = array[j];
+		array[j] = array[index];
+		array[index] = t;
 	}
 }
